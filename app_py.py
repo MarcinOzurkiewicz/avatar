984d43b2ba258272123c1afb9de5ef365a4a5702
Marcin OZurkiewicz <mozurkiewicz@execoz.pl>
2024-06-12 12:16:41 +0200

    removed NAM logo from page, logo will be included in streamlit app

--- app_py.py
+++ app_py.py
@@ -2,8 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 def run():
-    iframe_src = "basic.html"
-    components.iframe(iframe_src)
+    iframe_src = "https://icy-sky-0f4e8cf03.5.azurestaticapps.net/"
+    components.iframe(iframe_src, height = 800, width = 800, scrolling = True)
    # You can add height and width to the component of course.
 
 if __name__ == "__main__":
